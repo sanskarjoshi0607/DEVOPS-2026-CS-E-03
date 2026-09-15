f194a6b4f1134cfc6d128cede8bd51be872d0aa3
SAMIR KUMAR <86513samir@gmail.com>
2026-09-15 09:04:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,55 +1,92 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const PORT = 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
+app.use(cors());
+app.use(express.json());
 
-    let filePath;
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
-    if (req.url === "/") {
-        filePath = path.join(__dirname, "index.html");
-    }
-    else if (req.url === "/style.css") {
-        filePath = path.join(__dirname, "style.css");
-    }
-    else if (req.url === "/script.js") {
-        filePath = path.join(__dirname, "script.js");
-    }
-    else {
-        res.writeHead(404);
-        res.end("404 - File Not Found");
-        return;
-    }
+// User Schema
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+  })
+);
 
-    fs.readFile(filePath, (error, data) => {
+// Quiz Schema
+const Quiz = mongoose.model(
+  "Quiz",
+  new mongoose.Schema({
+    title: String,
+    questions: [
+      {
+        question: String,
+        options: [String],
+        correctAnswer: String
+      }
+    ]
+  })
+);
 
-        if (error) {
-            res.writeHead(500);
-            res.end("500 - Server Error");
-            return;
-        }
-
-        let contentType = "text/html";
-
-        if (filePath.endsWith(".css")) {
-            contentType = "text/css";
-        }
-
-        if (filePath.endsWith(".js")) {
-            contentType = "text/javascript";
-        }
-
-        res.writeHead(200, {
-            "Content-Type": contentType
-        });
-
-        res.end(data);
-    });
-
+// Home Route
+app.get("/", (req, res) => {
+  res.send("Quiz Portal Backend Running");
 });
 
-server.listen(PORT, () => {
-    console.log(`Quiz App Server running at http://localhost:${PORT}`);
+// Register User
+app.post("/register", async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+});
+
+// Create Quiz
+app.post("/quiz", async (req, res) => {
+  const quiz = await Quiz.create(req.body);
+  res.json(quiz);
+});
+
+// Get All Quizzes
+app.get("/quiz", async (req, res) => {
+  const quizzes = await Quiz.find();
+  res.json(quizzes);
+});
+
+// Get Single Quiz
+app.get("/quiz/:id", async (req, res) => {
+  const quiz = await Quiz.findById(req.params.id);
+  res.json(quiz);
+});
+
+// Submit Quiz
+app.post("/submit/:id", async (req, res) => {
+  const quiz = await Quiz.findById(req.params.id);
+
+  let score = 0;
+
+  quiz.questions.forEach((q, index) => {
+    if (req.body.answers[index] === q.correctAnswer) {
+      score++;
+    }
+  });
+
+  res.json({
+    totalQuestions: quiz.questions.length,
+    score: score
+  });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
